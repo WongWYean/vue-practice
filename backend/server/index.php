a2f4a178030c7e1f333180bf7102c1f1b6a45dc8
WongWYean <wongwyean3@gmail.com>
2025-02-17 09:25:52 +0800

    Add PHP debugger environment

--- backend/server/index.php
+++ backend/server/index.php
@@ -8,6 +8,9 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+// print phpinfo here
+echo phpinfo();
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit;
